b3e0008b10050b5f147df8d20dd064bbe66bdf3a
unknown <dlo21@pitt.edu>
2025-01-26 17:33:51 -0500

    wroteMoreCode

--- model.py
+++ model.py
@@ -6,6 +6,12 @@ import json
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 data = pd.read_json('newdata.json')
 
 print(data.head())
@@ -28,3 +34,24 @@ def preprocess_text(text):
 
 # Preprocess
 data['processed_review'] = data['reviewText'].apply(preprocess_text)
+
+vectorizer = TfidfVectorizer()
+
+X = vectorizer.fit_transform(data['processed_review'])
+
+print(X.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, data['overall'], test_size=0.2, random_state=42)
+
+
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.4f}')
+if accuracy >= .80:
+    print("Yippee")
+
