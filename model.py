f150c29a4a0426e77859e49ff13f49c8e55cf052
unknown <dlo21@pitt.edu>
2025-01-27 01:05:11 -0500

    Added thoughtful insight

--- model.py
+++ model.py
@@ -14,7 +14,8 @@ from sklearn.metrics import accuracy_score, matthews_corrcoef
 
 data = pd.read_json('newdata.json')
 
-print(data.head())
+# I'm good to delete this line
+# print(data.head())
 
 #nltk.download('stopwords')
 
@@ -40,12 +41,24 @@ vectorizer = TfidfVectorizer()
 
 X = vectorizer.fit_transform(data['processed_review'])
 
-print(X.shape)
+# I'm good to delete this
+# print(X.shape)
 
 X_train, X_test, y_train, y_test = train_test_split(X, data['overall'], test_size=0.2, random_state=42)
 
 
 model = LogisticRegression(max_iter=1000)
+# The following line penalizes the model for making mistakes on minority classes
+# This can be useful because we have very imbalanced data (I think)
+# The model focuses more on minority classes, 
+#       which reduces accuracy (as it's more focused on the less likely cases)
+# but does improve the mcc, which cares about the classes in a more equal way 
+# So, accuracy goes from .69 -> .57 but mcc goes from .22 -> .27
+# Overarching question is: "Which metrics do we care about? Which ones mean our model is better?"
+#       As a side thing, we could try seeing what the metrics for something pre-built is (BERT, HuggingFace etc), and use their 
+#       performance in these stats to get a general idea of what we're aiming for 
+#       (eg, if BERT has low accuracy but high mcc, maybe that means the model is better, if we can justify this)
+# model = LogisticRegression(class_weight='balanced', max_iter=1000)
 
 model.fit(X_train, y_train)
 
