53fe8993c1d547c888ba9d6dc166639687622420
c-c-cole <coe18@pitt.edu>
2025-01-27 00:30:05 -0500

    mcc categories

--- model.py
+++ model.py
@@ -53,6 +53,20 @@ y_pred = model.predict(X_test)
 
 mcc = matthews_corrcoef(y_test, y_pred)
 print("Matthews Correlation Coefficient:", mcc)
+if mcc == 1:
+    print("Perfectly Reliable")
+elif 0.70 <= mcc < 1.0:
+    print("Excellent Reliability")
+elif 0.50 <= mcc < 0.7:
+    print("Good Reliability")
+elif 0.30 <= mcc < 0.5:
+    print("Moderate Reliability")
+elif 0.00 < mcc < 0.3:
+    print("Weak Reliability")
+elif mcc == 0:
+    print("Model performs no better than random predictions")
+else:
+    print("Model predictions inversely related to actual outcomes")
 
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy:.4f}')
